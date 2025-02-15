a2d5177b8b8d9dc012f281dcf72d8139b8f9d617
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2025-02-15 06:59:50 -0300

    🚧 criação arquivos

---
+++ python/rede/classificador.py
@@ -0,0 +1,55 @@
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
+from scipy import signal
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+tf.get_logger().setLevel('ERROR')
+
+# Carregar o modelo
+modelo_carregado = load_model('./backup/classificador-80.h5')
+
+def getHarmonicos(dados, amostras, T, qtd_Peaks=7):
+    L = []
+    for i in range(0, len(dados)):
+        df = dados.iloc[i,:].transpose()
+
+        # Faz a transformada rápida de fourrier
+        fft = np.fft.fft(df)
+
+        # Faz a transformada rápida de fourrier
+        fast = np.fft.fftfreq(amostras, T)
+
+        # Seleciona a primeira metade da sequência e normaliza as amplitudes pelo número de amostras (divide por N)
+        freqs = fast[:amostras//2]
+
+        # Pega amplitudes
+        amplet = np.abs(fft)[:amostras//2] / amostras
+        amplet = np.log10(amplet)*20
+
+        pontos = signal.argrelextrema(amplet, np.greater)
+        pontos = pontos[0]
+
+        peak_x = list(np.abs(freqs[pontos]))
+        peak_y = list(np.abs(amplet[pontos]))
+
+        lista = []
+        for j in range(0, qtd_Peaks, 1):
+            lista.append(peak_x[j])
+
+        for j in range(0, qtd_Peaks, 1):
+            lista.append(peak_y[j])
+
+        L.append(lista)
+        
+        scaler = MinMaxScaler(feature_range=(0,1))
+    return scaler.fit_transform(pd.DataFrame(L)) #type: ignore
+
+def getClasse(dados, amostras, T):
+    harmonicos_normalizados = getHarmonicos(dados, amostras, T)
+    
+    predictions = modelo_carregado.predict(harmonicos_normalizados)
+    
+    classe = pd.DataFrame(predictions) 
+    classe = classe.idxmax(axis=1)[0] + 1 # pega coluna com valor mais proximo de 1
+    return classe
