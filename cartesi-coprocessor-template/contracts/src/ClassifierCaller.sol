0f6e0b5e75c8d122947071735448fa438ef2cc20
Arthur Coelho Estevão <56831082+arthurcoelho442@users.noreply.github.com>
2025-02-22 09:54:02 -0300

    🚧 ajuste contrato

--- cartesi-coprocessor-template/contracts/src/ClassifierCaller.sol
+++ cartesi-coprocessor-template/contracts/src/ClassifierCaller.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "../lib/coprocessor-base-contract/src/CoprocessorAdapter.sol";
+import "./lib/coprocessor-base-contract/src/CoprocessorAdapter.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract ClassifierCaller is CoprocessorAdapter, AccessControl {
@@ -13,23 +13,20 @@ contract ClassifierCaller is CoprocessorAdapter, AccessControl {
         string name;
         uint256  id;
     }
-    struct Device {
-        string name;
-        uint256  id;
-        DeviceReportView[] data;
-    }
-
     struct User {
         string name;
-        Device[]  devices;
+        DeviceView[]  devices;
     }
 
     address public owner;
     address[] public userAddresses;
     mapping(address => User) public users;
+    mapping(address => mapping(uint256 => DeviceReportView[]))
+            public deviceData;
+
     mapping(bytes32 => address) private requestSender;
     mapping(bytes32 => uint256) private requestTimestamp;
-
+    
     event ResultReceived(bytes32 indexed inputPayloadHash, bytes output);
 
     modifier onlySuperAdmin() {
@@ -47,37 +44,32 @@ contract ClassifierCaller is CoprocessorAdapter, AccessControl {
         users[owner].name = 'owner';
 
         // Add devices using push
-        users[owner].devices.push(Device('Fan', 10, new DeviceReportView[](0)));
-        users[owner].devices.push(Device('Hair cutter', 13, new DeviceReportView[](0)));
-        users[owner].devices.push(Device('Notebook Wanderley', 14, new DeviceReportView[](0)));
-        users[owner].devices.push(Device('Notebook Leo', 15, new DeviceReportView[](0)));
+        users[owner].devices.push(DeviceView('Fan', 10));
+        users[owner].devices.push(DeviceView('Hair cutter', 13));
+        users[owner].devices.push(DeviceView('Notebook Wanderley', 14));
+        users[owner].devices.push(DeviceView('Notebook Leo', 15));
 
         // ADMIN
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
     }
 
-    function runExecution(bytes calldata input) {
+    function runExecution(bytes memory input) internal {
         callCoprocessor(input);
     }
 
     function handleNotice(bytes32 inputPayloadHash, bytes memory notice) internal override {
-        require(notice.length >= 64, "Invalid notice length");
-
-        (uint256 id, uint256 current) = abi.decode(notice, (uint256, uint256));
-
         address sender      = requestSender[inputPayloadHash];
         uint256 timestamp   = requestTimestamp[inputPayloadHash];
         require(sender != address(0), "Unknown request");
 
+        require(notice.length >= 64, "Invalid notice length");
+
+        (uint256 id, uint256 current) = abi.decode(notice, (uint256, uint256));
+        deviceData[sender][id].push(DeviceReportView(current, timestamp));
+
         delete requestSender[inputPayloadHash];
         delete requestTimestamp[inputPayloadHash];
 
-        for (uint i = 0; i < users[sender].devices.length; i++) {
-            if (users[sender].devices[i].id == id) {
-                users[sender].devices[i].data.push(DeviceReportView(current, timestamp));
-                break;
-            }
-        }
         emit ResultReceived(inputPayloadHash, notice);
     }
 
@@ -101,7 +93,7 @@ contract ClassifierCaller is CoprocessorAdapter, AccessControl {
         DeviceView[] memory deviceView = new DeviceView[](devicesCount);
 
         for (uint i = 0; i < devicesCount; i++) {
-            Device memory device = users[user].devices[i];
+            DeviceView memory device = users[user].devices[i];
             deviceView[i] = DeviceView(device.name, device.id);
         }
         return deviceView;
@@ -109,12 +101,12 @@ contract ClassifierCaller is CoprocessorAdapter, AccessControl {
     function getDeviceCurrentData(address user, uint256 id) public view userExists(user) returns (DeviceReportView[] memory) {
         for (uint i = 0; i < users[user].devices.length; i++) {
             if (users[user].devices[i].id == id){
-                uint256 dataLength = users[user].devices[i].data.length;
+                uint256 dataLength = deviceData[user][id].length;
                 uint256 init = dataLength >= 60 ? dataLength - 60 : 0;
                 DeviceReportView[] memory data = new DeviceReportView[](dataLength - init);
 
                 for (uint j = 0; j < dataLength - init; j++) {
-                    data[j] = users[user].devices[i].data[init + j];
+                    data[j] = deviceData[user][id][init + j];
                 }
 
                 return data;
@@ -133,6 +125,6 @@ contract ClassifierCaller is CoprocessorAdapter, AccessControl {
         for (uint i = 0; i < users[user].devices.length; i++) {
             require(users[user].devices[i].id != id, "Device already exists");
         }
-        users[user].devices.push(Device(name, id, new DeviceReportView[](0)));
+        users[user].devices.push(DeviceView(name, id));
     }
 }
